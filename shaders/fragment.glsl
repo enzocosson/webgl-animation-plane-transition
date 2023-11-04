4302f4ff642189b236f6684184a0cd0ed07a07a0
enzocosson <enzolemercier@gmail.com>
2023-11-05 00:26:29 +0100

    Fix : resize viewport

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -1,20 +1,30 @@
 uniform float time;
-uniform sampler2D uTexture;
 uniform float uProgress;
-varying float pulse;
+uniform vec2 uTextureSize;
+uniform sampler2D uTexture;
 varying vec2 vUv;
-varying vec3 vNormal;
+
+varying vec2 vSize;
+
+vec2 getUV(vec2 uv, vec2 textureSize, vec2 quadSize) {
+    vec2 tempUV = uv - vec2(0.5);
+
+    float quadAspect = quadSize.x / quadSize.y;
+    float textureAspect = textureSize.x / textureSize.y;
+    if(quadAspect < textureAspect) {
+        tempUV = tempUV * vec2(quadAspect / textureAspect, 1.);
+    } else {
+        tempUV = tempUV * vec2(1., (textureAspect / quadAspect));
+    }
+
+    tempUV += vec2(0.5);
+    return tempUV;
+}
 
 void main() {
-    // gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);
-    // vec4 myimage = texture(uTexture, vUv + 0.01 * sin(vUv * 20. + time));
 
-    // float sinPulse = (1. + sin(vUv.x * 50. + time)) * 0.5;
-    // gl_FragColor = vec4(vUv, 0.0, 1.0);
-    // gl_FragColor = vec4(sinPulse, 0.0, 0.0, 1.0);
-    // gl_FragColor = vec4(0.5 * (pulse + 1.), 0., 0., 1.0);
-    vec4 myimage = texture(uTexture, vUv);
-    gl_FragColor = vec4(vUv, 0.0, 1.0);
+    vec2 correctUV = getUV(vUv, uTextureSize, vSize);
+    vec4 myimage = texture(uTexture, correctUV);
+    gl_FragColor = vec4(vUv, 0., 1.);
     gl_FragColor = myimage;
-
 }
